9c0b173587f0869cf06d93d86489e5cbbff9b85f
manoj5864 <manoj.mahabaleshwar@tum.de>
2017-08-22 23:49:24 +0200

    syncpipes client as a service

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,14 @@
 
 var gulp = require('gulp');
 var wrench = require('wrench');
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'SyncPipes Client',
+  description: 'SyncPipes client as a windows service.',
+  script: 'app.js'
+});
 
 /**
  *  This will load all js or coffee files in the gulp directory
@@ -26,4 +34,33 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
  */
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
+});
+
+gulp.task('install:service', ['build'], function () {
+  // Listen for the "install" event, which indicates the
+  // process is available as a service.
+  svc.on('install',function(){
+    svc.start();
+  });
+
+  svc.on('alreadyinstalled',function(){
+    console.log('This service is already installed.');
+  });
+
+  svc.on('start',function(){
+    console.log(svc.name+' started!\nVisit http://127.0.0.1:3000 to see it in action.');
+  });
+
+  svc.install();
+});
+
+gulp.task('uninstall:service', ['build'], function () {
+  // Listen for the "uninstall" event so we know when it's done.
+  svc.on('uninstall',function(){
+    console.log('Uninstall complete.');
+    console.log('The service exists: ',svc.exists);
+  });
+
+  // Uninstall the service.
+  svc.uninstall();
 });
